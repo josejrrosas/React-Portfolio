b5b07d81612f6cf4a10aa49f061cb8be882753d7
Jose Rosas <josejrrosas@yahoo.com>
2022-03-05 18:21:22 -0600

    got tabs to work

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { StyledAbout, Logos, Image } from "../Components/styles/About.styled";
-import Tab from 'react-bootstrap/Tab'
-import Tabs from 'react-bootstrap/Tabs'
-import TabContainer from 'react-bootstrap/TabContainer'
-import TabContent from 'react-bootstrap/TabContent'
-import TabPane from 'react-bootstrap/TabPane'
-import Nav from 'react-bootstrap/Nav'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import TabContainer from "react-bootstrap/TabContainer";
+import TabContent from "react-bootstrap/TabContent";
+import TabPane from "react-bootstrap/TabPane";
+import Nav from "react-bootstrap/Nav";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import {
   SiHtml5,
@@ -29,106 +29,101 @@ const About = () => {
     <StyledAbout>
       <div id="about">
         <h1>Hello!</h1>
-        <p>
-          My name is Jose Rosas. I am a Web Developer from Dallas, Texas. I am
-          excited to apply my passion for learning to bust into the Web-Dev
-          industry! If I'm not studying, I am exercising, playing video games ,
-          or mindlessly watching youtube videos!
-        </p>
       </div>
 
-      <div id="education">
-        <div>
-          <h2>Education</h2>
-          <h3>Southern Methodist University</h3>
-          <p>Full-Stack Web Development Certificate</p>
-
-          <h3>University of Texas at Arlington</h3>
-          <p>Bachelors of Science in Information Systems</p>
-        </div>
-
-        <div>
-          <Image src={Uta} alt="Jose Rosas" />
-          <Image src={Smu} alt="Jose Rosas" />
-        </div>
-      </div>
-
-      <div id="skills">
-        <h2>Skills</h2>
-        <h3>Experience in many in-demand Web Development technologies!</h3>
-        <Logos>
-          <ul id="logos">
-            <li>
-              <SiHtml5 />
-              <p>HTML</p>
-            </li>
-            <li>
-              <SiCss3 />
-              <p>CSS</p>
-            </li>
-            <li>
-              <SiJavascript />
-              <p>Javascript</p>
-            </li>
-            <li>
-              <SiReact />
-              <p>React</p>
-            </li>
-            <li>
-              <SiNodedotjs />
-              <p>NodeJs</p>
-            </li>
-            <li>
-              <SiBootstrap />
-              <p>Bootstrap</p>
-            </li>
-            <li>
-              <SiMongodb />
-              <p>MongoDB</p>
-            </li>
-            <li>
-              <SiGit />
-              <p>Git</p>
-            </li>
-            <li>
-              <SiMysql />
-              <p>MySql</p>
-            </li>
-          </ul>
-        </Logos>
-      </div>
-
-      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      <Tab.Container id="left-tabs-example" defaultActiveKey="about">
         <Row>
           <Col sm={3}>
             <Nav variant="pills" className="flex-column">
               <Nav.Item>
-                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                <Nav.Link eventKey="about">About</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                <Nav.Link eventKey="education">Education</Nav.Link>
               </Nav.Item>
               <Nav.Item>
-                <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                <Nav.Link eventKey="skills">Skills</Nav.Link>
               </Nav.Item>
             </Nav>
           </Col>
           <Col sm={9}>
             <Tab.Content>
-
-              <Tab.Pane eventKey="first">
-                poop
+              <Tab.Pane eventKey="about">
+                <div id="about">
+                  <p>
+                    My name is Jose Rosas. I am a Web Developer from Dallas,
+                    Texas. I am excited to apply my passion for learning to bust
+                    into the Web-Dev industry! If I'm not studying, I am
+                    exercising, playing video games , or mindlessly watching
+                    youtube videos!
+                  </p>
+                </div>
               </Tab.Pane>
 
-              <Tab.Pane eventKey="second">
-                caca
+              <Tab.Pane eventKey="education">
+                <div id="education">
+                  <div>
+                    <h3>Southern Methodist University</h3>
+                    <p>Full-Stack Web Development Certificate</p>
+
+                    <h3>University of Texas at Arlington</h3>
+                    <p>Bachelors of Science in Information Systems</p>
+                  </div>
+
+                  <div>
+                    <Image src={Uta} alt="Jose Rosas" />
+                    <Image src={Smu} alt="Jose Rosas" />
+                  </div>
+                </div>
               </Tab.Pane>
 
-              <Tab.Pane eventKey="third">
-                pp
+              <Tab.Pane eventKey="skills">
+                <div id="skills">
+                  <h3>
+                    Experience in many in-demand Web Development technologies!
+                  </h3>
+                  <Logos>
+                    <ul id="logos">
+                      <li>
+                        <SiHtml5 />
+                        <p>HTML</p>
+                      </li>
+                      <li>
+                        <SiCss3 />
+                        <p>CSS</p>
+                      </li>
+                      <li>
+                        <SiJavascript />
+                        <p>Javascript</p>
+                      </li>
+                      <li>
+                        <SiReact />
+                        <p>React</p>
+                      </li>
+                      <li>
+                        <SiNodedotjs />
+                        <p>NodeJs</p>
+                      </li>
+                      <li>
+                        <SiBootstrap />
+                        <p>Bootstrap</p>
+                      </li>
+                      <li>
+                        <SiMongodb />
+                        <p>MongoDB</p>
+                      </li>
+                      <li>
+                        <SiGit />
+                        <p>Git</p>
+                      </li>
+                      <li>
+                        <SiMysql />
+                        <p>MySql</p>
+                      </li>
+                    </ul>
+                  </Logos>
+                </div>
               </Tab.Pane>
-
-
             </Tab.Content>
           </Col>
         </Row>
