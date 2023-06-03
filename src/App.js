e7cfe4007ba0c73456d4cc56a24fdd5fe11421cc
Jose Rosas <josejrrosas@yahoo.com>
2023-06-02 19:57:50 -0500

    adjusted path for home route

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
         <Toggle theme = {theme} setTheme = {setTheme} />
           <Nav />
         <Routes>
-          <Route path = "/" element={<Home />} />
+          <Route path = {["/","/react-portfolio"]} element={<Home />} />
           <Route path = "/about" element={<About />} />
           <Route path = "/projects" element={<Projects />} />
           <Route path = "/contact" element={<Contact />} />
